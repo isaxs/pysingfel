6db1428c05bf4282f79bfda1aba4c4bee98af30a
Zhaoyou Wang <zhxywzy@gmail.com>
2017-07-23 17:45:06 -0700

    add test readPDB

--- tests/test_particle.py
+++ tests/test_particle.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-from pysingfel.particle import *
+from pysingfel.radiationDamageMPI import *
 
 
 class particleTests(unittest.TestCase):
@@ -10,6 +10,28 @@ class particleTests(unittest.TestCase):
         p.readPDB('test_files/5g3x.pdb', ff='WK')
 
         self.assertTrue(isinstance(p.ffTable, np.ndarray))
+
+    def test_calFromPDB(self):
+        p = Particle()
+        p.readPDB('test_files/5g3x.pdb', ff='WK')
+
+        # generate and apply rotation to particle
+        quaternion = getRandomRotation('z')
+        rotateParticle(quaternion, p)
+
+        geomFile = 'test_files/s2e.geom'
+        beamFile = 'test_files/s2e_readPDB.beam'
+
+        det = Detector(geomFile)  # read geom file
+        beam = Beam(beamFile)  # read beam file (note that beam info must exist for reading pdb)
+        det.init_dp(beam)  # initialize diffraction pattern
+
+        detector_intensity = calculate_molecularFormFactorSq(p, det)
+        # Using LCLS AMO beamline fluence: 8.6480e+05 J/cm^2 (assuming pulse energy 4.1 mJ)
+        detector_intensity *= det.solidAngle * det.PolarCorr * 8.6480e9 / 1.6e-19
+        detector_counts = convert_to_poisson(detector_intensity)
+
+        self.assertTrue(isinstance(detector_counts, np.ndarray))
 
 if __name__ == '__main__':
     unittest.main()
